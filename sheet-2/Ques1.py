f0334d486d4e45ead78cf94cdf8bc5b6a7b246db
vanshnijhawan591 <vanshnijhawan591@gmail.com>
2026-02-17 10:38:38 +0530

    Implement kth smallest element using max heap

--- sheet-2/Ques1.py
+++ sheet-2/Ques1.py
@@ -2,8 +2,16 @@ class Solution {
   public:
     int kthSmallest(vector<int> &arr, int k) {
         
-        sort(arr.begin(), arr.end());
+        priority_queue<int> maxHeap;
         
-        return arr[k - 1];   // k-1 because indexing starts from 0
+        for(int i = 0; i < arr.size(); i++) {
+            maxHeap.push(arr[i]);
+            
+            if(maxHeap.size() > k) {
+                maxHeap.pop();
+            }
+        }
+        
+        return maxHeap.top();
     }
 };
